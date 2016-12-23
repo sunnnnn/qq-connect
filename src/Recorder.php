9323050fbe178c29909303cb4436b73ddc2a6000
sunnnnn <263021000@qq.com>
2016-12-23 16:30:50 +0800

    add 1.0.1

---
+++ src/Recorder.php
@@ -0,0 +1,52 @@
+<?php
+namespace sunnnnn\qqconnect;
+
+class Recorder{
+    private static $data;
+    private $inc;
+    private $error;
+
+    public function __construct(){
+        $this->error = new ErrorCase();
+
+        //-------读取配置文件
+        $this->inc = config('qqconnect');
+        if(empty($this->inc)){
+            $this->error->showError("20001");
+        }
+
+        if(empty(session('QC_userData'))){
+            self::$data = array();
+        }else{
+            self::$data = session('QC_userData');
+        }
+    }
+
+    public function write($name,$value){
+        self::$data[$name] = $value;
+    }
+
+    public function read($name){
+        if(empty(self::$data[$name])){
+            return null;
+        }else{
+            return self::$data[$name];
+        }
+    }
+
+    public function readInc($name){
+        if(empty($this->inc[$name])){
+            return null;
+        }else{
+            return $this->inc[$name];
+        }
+    }
+
+    public function delete($name){
+        unset(self::$data[$name]);
+    }
+
+    function __destruct(){
+        session('QC_userData', self::$data);
+    }
+}
